a32e8a7095844b0352dc5407a51baf3017c3da7a
Chase5ws <55267518+chase5ws@users.noreply.github.com>
2024-03-16 04:56:13 +0800

    Add files via upload

---
+++ AscCatSamurai-NFT/AscCatSamurai.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+
+/// @custom:security-contact chase5ws@gmail.com
+contract AscCatSamurai is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
+    uint256 private _nextTokenId;
+    string private _baseTokenURI;
+    uint256 maxSupply = 350;
+    bool public publicMintOpen = false;
+    bool public allowListMintOpen = false;
+    mapping(address => bool) public allowList;
+    address[] public allowListAddresses; // This array will keep track of the addresses
+
+
+
+    constructor(address initialOwner)
+        ERC721("asc cat samurai", "acs")
+        Ownable(initialOwner)
+    {}
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    // 允许合约所有者更新基本 URI
+    function setBaseURI(string memory newBaseURI) public onlyOwner {
+        _baseTokenURI = newBaseURI;
+    }
+
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // Modify the mint windows
+    function editMintWindows(
+        bool _publicMintOpen,
+        bool _allowListMintOpen
+    ) external onlyOwner {
+        publicMintOpen = _publicMintOpen;
+        allowListMintOpen = _allowListMintOpen;
+    }
+
+    // Populate the Allow List
+    function setAllowList(address[] calldata addresses) external onlyOwner {
+        for(uint256 i = 0; i < addresses.length; i++){
+            if (!allowList[addresses[i]]) { // Check if the address is not already in the allowList
+                allowList[addresses[i]] = true;
+                allowListAddresses.push(addresses[i]); // Add to the allowListAddresses array
+            }
+        }
+    }
+
+    // require only the allowList people to mint
+    // Add publicMint and allowListMintOpen Variables
+    function allowListMint() public payable {
+        require(allowListMintOpen, "Allowlist Mint Closed");
+        require(allowList[msg.sender], "You are not on the allow list");
+        require(msg.value == 0 ether, "Not Enough Funds");
+        internalMint();
+        // 鑄造成功後，直接將地址從允許列表中移除
+        allowList[msg.sender] = false;
+    }
+
+    // Add Payment
+    // Add limiting of supply
+    function publicMint() public payable {
+        require(publicMintOpen, "Public Mint Closed");
+        require(msg.value == 10 ether, "You are not on the allow list");
+    }
+
+    function internalMint() internal {
+        require(totalSupply() < maxSupply, "We Sold Out!");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
